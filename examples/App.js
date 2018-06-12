b853da3f2c21a7c4f91fd946919d8dc8921fbb34
wildlifehexagon <crossthebreeze@gmail.com>
2018-06-12 16:16:27 -0500

    remove navbar and console.log

--- examples/App.js
+++ examples/App.js
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import Tabs from '../src/Components/Tabs'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
-import { Panel, PanelBody, PanelTitle, PanelHeader } from 'dicty-components-panel'
-import { Navbar } from 'dicty-components-navbar'
-import styled from 'styled-components'
+import React from "react"
+import ReactDOM from "react-dom"
+import Tabs from "../src/Components/Tabs"
+import { BrowserRouter, Route, Link } from "react-router-dom"
+import {
+  Panel,
+  PanelBody,
+  PanelTitle,
+  PanelHeader,
+} from "dicty-components-panel"
+import styled from "styled-components"
 
 const Container = styled.div`
   display: flex;
@@ -12,229 +16,84 @@ const Container = styled.div`
   flex-direction: row;
 `
 const Home = () => {
-    return (
-        <Container>
-          <Link to='/gene/sadA'>
-            gene
-          </Link>
-          <Link to='/gene/sadA/blast'>
-            blast
-          </Link>
-          <Link to='/gene/sadA/kek'>
-            kek
-          </Link>
-          <Link to='/gene/sadA/test'>
-            test
-          </Link>
-        </Container>
-    )
+  return (
+    <Container>
+      <Link to="/gene/sadA">gene</Link>
+      <Link to="/gene/sadA/blast">blast</Link>
+      <Link to="/gene/sadA/kek">kek</Link>
+      <Link to="/gene/sadA/test">test</Link>
+    </Container>
+  )
 }
-const items = [
-    {
-        dropdown: true,
-        title: 'Genomes',
-        items: [
-            {
-                name: 'Genomes Home',
-                href: 'http://www.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Tools',
-        items: [
-            {
-                name: 'Tools Home',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'New Genome Browser',
-              href: 'http://wwww.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Explore',
-        items: [
-            {
-                name: 'Explore Home',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Dicty Art',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Gallery',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Genome Resources',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Genome Statistics',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Learn About Dicty',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Teaching Protocols',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Useful Links',
-              href: 'http://wwww.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Research',
-        items: [
-            {
-                name: 'Research Home',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Anatomy Ontology',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Codon Bias Table',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Nomenclature Guidelines',
-              href: 'http://wwww.google.com'
-            },
-            {
-                name: 'Phenotyping',
-                href: 'http://www.google.com'
-            },
-            {
-              name: 'Techniques',
-              href: 'http://wwww.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Dicty Stock Center',
-        items: [
-            {
-              name: 'Stock Center Home',
-              href: 'http://www.google.com'
-            }
-        ]
-    },
-    {
-        dropdown: true,
-        title: 'Community',
-        items: [
-            {
-              name: 'Community Home',
-              href: 'http://www.google.com'
-            },
-            {
-              name: 'Cite Us',
-              href: 'http://www.google.com'
-            },
-            {
-              name: 'Dicty Annual Conferences',
-              href: 'http://www.google.com'
-            }
-        ]
-    }
-]
 
 const Content = (title, content) => {
-    return (
-        <Panel collapse>
-          <PanelHeader>
-            <PanelTitle>
-              { title }
-            </PanelTitle>
-          </PanelHeader>
-          <PanelBody>
-            { content }
-          </PanelBody>
-        </Panel>
-    )
+  return (
+    <Panel collapse>
+      <PanelHeader>
+        <PanelTitle>{title}</PanelTitle>
+      </PanelHeader>
+      <PanelBody>{content}</PanelBody>
+    </Panel>
+  )
 }
 
 const tabs = [
-    {
-        title: 'Gene Summary',
-        element: (
-            <div>
-              { Content('Blast', 'Content Goes Here') }
-              { Content('Content 2', 'P1????????\nP2~~~~~~~~~~~~~\n\nP3!!!!!!!!!!!!!') }
-            </div>
-        ),
-        link: 'summary'
-    },
-    {
-        title: 'Protein Information',
-        element: (
-            <div style={ {height: 100, background: 'blue'} }></div>
-        ),
-        link: 'proteininformation'
-    },
-    {
-        title: 'Gene Ontology',
-        element: (
-            <div style={ {height: 100, background: 'green'} }></div>
-        ),
-        link: 'ontology'
-    },
-    {
-        title: 'Orthologs',
-        element: (
-            <div style={ {height: 100, background: 'goldenrod'} }></div>
-        ),
-        link: 'orthologs'
-    },
-    {
-        title: 'Phenotypes',
-        element: (
-            <div style={ {height: 100, background: 'goldenrod'} }></div>
-        ),
-        link: 'phenotypes'
-    },
-    {
-        title: 'Reference',
-        element: (
-            <div style={ {height: 100, background: 'goldenrod'} }></div>
-        ),
-        link: 'reference'
-    },
-    {
-        title: 'BLAST',
-        element: (
-            <div style={ {height: 100, background: 'goldenrod'} }></div>
-        ),
-        link: 'blast'
-    }
+  {
+    title: "Gene Summary",
+    element: (
+      <div>
+        {Content("Blast", "Content Goes Here")}
+        {Content("Content 2", "P1????????\nP2~~~~~~~~~~~~~\n\nP3!!!!!!!!!!!!!")}
+      </div>
+    ),
+    link: "summary",
+  },
+  {
+    title: "Protein Information",
+    element: <div style={{ height: 100, background: "blue" }} />,
+    link: "proteininformation",
+  },
+  {
+    title: "Gene Ontology",
+    element: <div style={{ height: 100, background: "green" }} />,
+    link: "ontology",
+  },
+  {
+    title: "Orthologs",
+    element: <div style={{ height: 100, background: "goldenrod" }} />,
+    link: "orthologs",
+  },
+  {
+    title: "Phenotypes",
+    element: <div style={{ height: 100, background: "goldenrod" }} />,
+    link: "phenotypes",
+  },
+  {
+    title: "Reference",
+    element: <div style={{ height: 100, background: "goldenrod" }} />,
+    link: "reference",
+  },
+  {
+    title: "BLAST",
+    element: <div style={{ height: 100, background: "goldenrod" }} />,
+    link: "blast",
+  },
 ]
 
 const Routes = () => {
-    return (
-        <BrowserRouter>
-          <div>
-            <Navbar items={ items } />
-            <Route exact path='/' component={ () => <Home /> } />
-            <Route path='/gene/:id' component={ (defaultProps) => { return <Tabs  { ...defaultProps} tabs={ tabs } /> } } />
-          </div>
-        </BrowserRouter>
-    )
+  return (
+    <BrowserRouter>
+      <div>
+        <Route exact path="/" component={() => <Home />} />
+        <Route
+          path="/gene/:id"
+          component={defaultProps => {
+            return <Tabs {...defaultProps} tabs={tabs} />
+          }}
+        />
+      </div>
+    </BrowserRouter>
+  )
 }
 
-ReactDOM.render(
-    <Routes />,
-    document.getElementById('root')
-)
+ReactDOM.render(<Routes />, document.getElementById("root"))
--- src/Components/Tabs.js
+++ src/Components/Tabs.js
@@ -1,9 +1,9 @@
 // @flow
-import React, { Component } from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import { lighten } from 'polished'
-import { Link as RouterLink, Route, Redirect, Switch } from 'react-router-dom'
-import Pane from './Pane'
+import React, { Component } from "react"
+import styled, { ThemeProvider } from "styled-components"
+import { lighten } from "polished"
+import { Link as RouterLink, Route, Redirect, Switch } from "react-router-dom"
+import Pane from "./Pane"
 
 const Container = styled.div`
   padding: 20px;
@@ -11,11 +11,11 @@ const Container = styled.div`
   position: relative;
 
   @media (max-width: 917px) {
-      display: flex;
-      flex-direction: column;
-      width: 100%;
-      padding: 20px 0px 0px 0px;
-      margin-top: 10px;
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    padding: 20px 0px 0px 0px;
+    margin-top: 10px;
   }
 `
 const TabBar = styled.div`
@@ -33,12 +33,10 @@ const TabBar = styled.div`
     position: initial;
     display: flex;
     max-width: 100%;
-    ${''/* border-top: 1px solid black; */}
-    -ms-overflow-style: none;
+    ${"" /* border-top: 1px solid black; */} -ms-overflow-style: none;
     overflow: -moz-scrollbars-none;
     &::-webkit-scrollbar {
-      ${''/* width: 0 !important; */}
-      display: none;
+      ${"" /* width: 0 !important; */} display: none;
     }
   }
 `
@@ -48,25 +46,30 @@ const Tab = styled.div`
   align-items: center;
   cursor: pointer;
   white-space: nowrap;
-  background: ${
-    props => {
-        return props.active ?
-        props.theme.primary ? props.theme.primary : '#15317e'
-        : props.theme.secondary ? props.theme.secondary : '#A3BAE9'
-    }
-  };
+  background: ${props => {
+    return props.active
+      ? props.theme.primary
+        ? props.theme.primary
+        : "#15317e"
+      : props.theme.secondary
+        ? props.theme.secondary
+        : "#A3BAE9"
+  }};
   border-top: 1px solid black;
   border-right: 1px solid black;
   border-left: 1px solid black;
   border-top-right-radius: 2px;
   border-top-left-radius: 2px;
-  height: ${ props => props.active ? '45px' : '35px'};
+  height: ${props => (props.active ? "45px" : "35px")};
   margin-right: 3px;
   margin-top: 1px;
   transition: all 0.17s ease;
 
   &:hover {
-    background: ${ props => !props.active && props.theme.secondary ? lighten(0.1, props.theme.secondary) : '#15317e' };
+    background: ${props =>
+      !props.active && props.theme.secondary
+        ? lighten(0.1, props.theme.secondary)
+        : "#15317e"};
   }
 
   @media (max-width: 917px) {
@@ -82,12 +85,12 @@ const RightArrow = styled.div`
   right: 0;
   background: rgba(0, 0, 0, 0.4);
   z-index: 1;
-  opacity: ${ props => props.visible ? 1 : 0 };
+  opacity: ${props => (props.visible ? 1 : 0)};
   transition: opacity 0.25s ease;
   pointer-events: none;
 
   &:after {
-    content: '';
+    content: "";
     position: absolute;
     width: 0;
     height: 0;
@@ -114,12 +117,12 @@ const LeftArrow = styled.div`
   left: 0;
   background: rgba(0, 0, 0, 0.4);
   z-index: 1;
-  opacity: ${ props => props.visible ? 1 : 0 };
+  opacity: ${props => (props.visible ? 1 : 0)};
   transition: opacity 0.25s ease;
   pointer-events: none;
 
   &:after {
-    content: '';
+    content: "";
     position: absolute;
     width: 0;
     height: 0;
@@ -142,7 +145,7 @@ const Link = styled(RouterLink)`
   height: 100%;
   width: 100%;
   display: flex;
-  padding: ${ props => props.active ? '10px 20px' : '10px 15px' };
+  padding: ${props => (props.active ? "10px 20px" : "10px 15px")};
   transition: all 0.17s ease;
   justify-content: center;
   align-items: center;
@@ -155,11 +158,14 @@ const Link = styled(RouterLink)`
 `
 const Text = styled.p`
   display: block;
-  color: ${
-    props => props.active
-      ? props.theme.tabText ? props.theme.tabText : 'white'
-      : props.theme.secondaryTabText ? props.theme.secondaryTabText : 'black'
-  };
+  color: ${props =>
+    props.active
+      ? props.theme.tabText
+        ? props.theme.tabText
+        : "white"
+      : props.theme.secondaryTabText
+        ? props.theme.secondaryTabText
+        : "black"};
 `
 const Content = styled.div`
   position: relative;
@@ -177,115 +183,130 @@ const Content = styled.div`
 `
 
 const theme = {
-    primary: '#15317e',
-    secondary: '#A3BAE9',
-    tabText: 'white'
+  primary: "#15317e",
+  secondary: "#A3BAE9",
+  tabText: "white",
 }
 
 type Props = {
-    tabs: Array<Object>,
-    location: Object,
-    match: Object
+  tabs: Array<Object>,
+  location: Object,
+  match: Object,
 }
 type State = {
-    scrollPos: ?string
+  scrollPos: ?string,
 }
 export default class Tabs extends Component {
-    displayName = 'Tabs'
-    content: any
-    tabBar: any
-    props: Props
-    state: State
+  displayName = "Tabs"
+  content: any
+  tabBar: any
+  props: Props
+  state: State
 
-    constructor() {
-        super()
-        this.state = {
-            scrollPos: 'left'
-        }
+  constructor() {
+    super()
+    this.state = {
+      scrollPos: "left",
     }
-    componentDidMount() {
-        this.tabBar.addEventListener('scroll', this.handleScroll)
-        window.addEventListener('resize', this.handleResize)
+  }
+  componentDidMount() {
+    this.tabBar.addEventListener("scroll", this.handleScroll)
+    window.addEventListener("resize", this.handleResize)
+  }
+  renderTabs = (): Array<?React$Element<any>> => {
+    const { tabs, match } = this.props
+    const { pathname } = this.props.location
+    const loc = pathname.split("/")[pathname.split("/").length - 1]
+    return tabs.map((tab, i) => {
+      const active = loc === tab.link ? true : false
+      return (
+        <Tab key={i} active={active}>
+          <Link to={`${match.url}/${tab.link}`} active={active}>
+            <Text active={active}>{tab.title}</Text>
+          </Link>
+        </Tab>
+      )
+    })
+  }
+  fromTop = (): boolean => {
+    const { location, tabs } = this.props
+    const loc = location.pathname.split("/")[
+      location.pathname.split("/").length - 1
+    ]
+    for (let i = 0; i < tabs.length; i++) {
+      if (loc === tabs[i].link) {
+        return true
+      }
     }
-    renderTabs = (): Array<?React$Element<any>> => {
-        const { tabs, match } = this.props
-        const { pathname } = this.props.location
-        const loc = pathname.split('/')[pathname.split('/').length - 1]
-        return tabs.map((tab, i) => {
-            const active = loc === tab.link ? true : false
-            return (
-              <Tab
-                key={ i }
-                active={ active }
-              >
-                <Link to={ `${match.url}/${tab.link}` } active={ active }>
-                  <Text active={ active }>
-                    { tab.title }
-                  </Text>
-                </Link>
-              </Tab>
-            )
-        })
+    return false
+  }
+  handleScroll = (): void => {
+    const { scrollPos } = this.state
+    const max = this.tabBar.scrollWidth - this.tabBar.clientWidth
+    if (this.tabBar.scrollLeft < 15 && scrollPos != "left") {
+      this.setState({
+        scrollPos: "left",
+      })
+    } else if (this.tabBar.scrollLeft > max - 15 && scrollPos != "right") {
+      this.setState({
+        scrollPos: "right",
+      })
+    } else if (
+      this.tabBar.scrollLeft > 15 &&
+      this.tabBar.scrollLeft < max - 15
+    ) {
+      this.setState({
+        scrollPos: "middle",
+      })
     }
-    fromTop = (): boolean => {
-        const { location, tabs } = this.props
-        const loc = location.pathname.split('/')[location.pathname.split('/').length - 1]
-        for (let i = 0; i < tabs.length; i++) {
-            if (loc === tabs[i].link) {
-                return true
-            }
-        }
-        return false
+  }
+  handleResize = (e: Event): void => {
+    const { scrollPos } = this.state
+    if (
+      this.tabBar.scrollWidth <= this.tabBar.clientWidth &&
+      scrollPos != "none"
+    ) {
+      this.setState({
+        scrollPos: "none",
+      })
+    } else if (scrollPos != "none") {
+      this.handleScroll()
     }
-    handleScroll = (): void => {
-        const { scrollPos } = this.state
-        const max = this.tabBar.scrollWidth - this.tabBar.clientWidth
-        if (this.tabBar.scrollLeft < 15 && scrollPos != 'left') {
-            this.setState({
-                scrollPos: 'left'
-            })
-        } else if (this.tabBar.scrollLeft > max - 15 && scrollPos != 'right') {
-            this.setState({
-                scrollPos: 'right'
-            })
-        } else if (this.tabBar.scrollLeft > 15 && this.tabBar.scrollLeft < max - 15) {
-            this.setState({
-                scrollPos: 'middle'
-            })
-        }
-    }
-    handleResize = (e: Event): void => {
-        const { scrollPos } = this.state
-        if (this.tabBar.scrollWidth <= this.tabBar.clientWidth && scrollPos != 'none') {
-            console.log('kek');
-            this.setState({
-                scrollPos: 'none'
-            })
-        } else if (scrollPos != 'none') {
-            this.handleScroll()
-        }
-    }
-    componentWillUnmount() {
-        this.tabBar.removeEventListener('scroll', this.handleScroll)
-        window.removeEventListener('resize', this.handleResize)
-    }
-    render() {
-        const { match, tabs, location } = this.props
-        let { scrollPos } = this.state
-        return (
-            <ThemeProvider theme={ theme }>
-              <Container>
-                { !this.fromTop() && <Redirect to={ `${match.url}/${tabs[0].link}` } /> }
-                <TabBar innerRef={ el => this.tabBar = el }>
-                  <LeftArrow visible={ scrollPos != 'left' && scrollPos != 'none' ? true : false } pos="left" />
-                  { this.renderTabs() }
-                  <RightArrow visible={ scrollPos != 'right' && scrollPos != 'none' ? true : false } pos="right" />
-                </TabBar>
-                <Content innerRef={ el => this.content = el }>
-                  <Route path={ `${match.url}/:tab` } component={ (props) => <Pane { ...props } tabs={ tabs } /> } />
-                </Content>
-              </Container>
-            </ThemeProvider>
-        )
-    }
+  }
+  componentWillUnmount() {
+    this.tabBar.removeEventListener("scroll", this.handleScroll)
+    window.removeEventListener("resize", this.handleResize)
+  }
+  render() {
+    const { match, tabs, location } = this.props
+    let { scrollPos } = this.state
+    return (
+      <ThemeProvider theme={theme}>
+        <Container>
+          {!this.fromTop() && <Redirect to={`${match.url}/${tabs[0].link}`} />}
+          <TabBar innerRef={el => (this.tabBar = el)}>
+            <LeftArrow
+              visible={
+                scrollPos != "left" && scrollPos != "none" ? true : false
+              }
+              pos="left"
+            />
+            {this.renderTabs()}
+            <RightArrow
+              visible={
+                scrollPos != "right" && scrollPos != "none" ? true : false
+              }
+              pos="right"
+            />
+          </TabBar>
+          <Content innerRef={el => (this.content = el)}>
+            <Route
+              path={`${match.url}/:tab`}
+              component={props => <Pane {...props} tabs={tabs} />}
+            />
+          </Content>
+        </Container>
+      </ThemeProvider>
+    )
+  }
 }
